3d4f45eae9fe2807aab49ebf4ca20e1d149eb068
s.lantsev <lantsev1981@mail.ru>
2025-08-13 16:43:13 +0200

    Добавил дату партии

--- Domain/Set.cs
+++ Domain/Set.cs
@@ -5,5 +5,6 @@ public record Set(byte Num, byte WonPoint, byte LostPoint, string WinnerLogin)
     public Guid MatchId { get; init; }
     public Match Match { get; init; } = null!;
     public byte Points => (byte)(WonPoint + LostPoint);
+    public DateTimeOffset Date { get; init; } = DateTimeOffset.UtcNow;
     public byte GetPoints(string login) => WinnerLogin == login ? WonPoint : LostPoint;
 }
